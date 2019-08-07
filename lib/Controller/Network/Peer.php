3c5f9bf6a030c460fb83191c912d951077b259c1
openthc <code@openthc.com>
2019-08-07 16:00:32 +0000

    fix apache example, patch response for peer

--- lib/Controller/Network/Peer.php
+++ lib/Controller/Network/Peer.php
@@ -12,19 +12,19 @@ class Peer
 
 		$peer = $REQ->getAttribute('peer');
 		if (empty($peer)) {
-			return $RES->withJSON(array(
-				'status' => 'failure',
-				'detail' => 'Peer Not Identified, check DNS A, AAAA, PTR and TXT',
-			), 400, JSON_PRETTY_PRINT);
+			return $RES->withJSON([
+				'meta' => [ 'detail' => 'Peer Not Identified, check DNS A, AAAA, PTR and TXT [CNP#016]' ],
+				'data' => [],
+			], 400, JSON_PRETTY_PRINT);
 		}
 		//var_dump($peer);
 
 		$peer_domain = $REQ->getAttribute('peer_domain');
 		if (empty($peer_domain)) {
-			return $RES->withJSON(array(
-				'status' => 'failure',
-				'detail' => 'Peer Not Identified, check DNS A, AAAA, PTR and TXT',
-			), 400, JSON_PRETTY_PRINT);
+			return $RES->withJSON([
+				'meta' => [ 'detail' => 'Peer Not Identified, check DNS A, AAAA, PTR and TXT [CNP#025]' ],
+				'data' => [],
+			], 400, JSON_PRETTY_PRINT);
 		}
 		//var_dump($peer_domain);
 
@@ -33,32 +33,40 @@ class Peer
 		$p1 = $REQ->getAttribute('peer_openthc');
 
 		if ($p0 != $p1) {
-			return $RES->withJSON(array(
-				'status' => 'failure',
-				'detail' => 'CNP#046: PTR and TXT records do not match',
-				'result' => array(
+			return $RES->withJSON([
+				'meta' => [
+					'detail' => 'PTR and TXT records do not match [CNP#046]',
+				],
+				'data' => [
 					'peer' => $peer,
 					'domain' => $peer_domain,
 					//'dns-keybase' => $
 					'dns-openthc' => $p1,
-				)
-			), 400, JSON_PRETTY_PRINT);
+				]
+			], 400, JSON_PRETTY_PRINT);
 		}
 
 		$file = sprintf('%s/var/network/%s/public.json', APP_ROOT, $peer_domain);
 		$data = json_encode(array(
 			'peer' => $peer,
 			'domain' => $peer_domain,
+			'secret' => $REQ->getAttribute('peer_secret'),
 			'keybase' => $REQ->getAttribute('peer_keybase'),
 			'openthc' => $REQ->getAttribute('peer_openthc'),
 		));
 
+		// Make Directory?
+		$path = dirname($file);
+		if (!is_dir($path)) {
+			mkdir($path, 0755, true);
+		}
+
 		file_put_contents($file, $data);
 
-		return $RES->withJSON(array(
-			'status' => 'success',
-			'detail' => 'Peer Registered',
-		));
+		return $RES->withJSON([
+			'meta' => [ 'detail' => 'Peer Registered' ],
+			'data' => $REQ->getAttribute('peer_secret')
+		]);
 
 	}
 
